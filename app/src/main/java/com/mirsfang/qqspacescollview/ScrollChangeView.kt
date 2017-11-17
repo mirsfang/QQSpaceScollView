c91390f2b47a38cd997ad7a333392ca4f2e46f4d
fang <mirsfang@163.com>
2017-11-17 19:51:11 +0800

    优化绘制性能

--- app/src/main/java/com/mirsfang/qqspacescollview/ScrollChangeView.kt
+++ app/src/main/java/com/mirsfang/qqspacescollview/ScrollChangeView.kt
@@ -3,13 +3,10 @@ package com.mirsfang.qqspacescollview
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffXfermode
-import android.graphics.RectF
+import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
 import android.util.DisplayMetrics
@@ -26,20 +23,22 @@ import junit.framework.AssertionFailedError
 
 class ScrollChangeView : View {
 
-    private var paint: Paint? = null
+    private lateinit var paint: Paint
     private var screenHeight: Int = 0
-    private var topCanVas: Canvas? = null
+    private var screenWidth: Int = 0
+    private lateinit var topCanVas: Canvas
     private var mHeight: Int = 0
     private var mWidth: Int = 0
     private var radius = 0f
-    private var rectF: RectF? = null
-    private var bottom: Bitmap? = null
-    private var topBg: Bitmap? = null
-    private var top: Bitmap? = null
+    private lateinit var rectF: RectF
+    private lateinit var bottom: Bitmap
+    private lateinit var topBg: Bitmap
+    private lateinit var top: Bitmap
     //X 和 Y 的偏移量
     private var offsetX = 100f
     private var offsetY = 100f
     private var images = intArrayOf(R.mipmap.draw1, R.mipmap.draw2)
+
 
     constructor(context: Context) : super(context) {
         init()
@@ -53,42 +52,52 @@ class ScrollChangeView : View {
     private fun init() {
         getWidthAndHeight()
         paint = Paint()
-        paint!!.alpha = 0
-        paint!!.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
-        paint!!.style = Paint.Style.FILL
-        paint!!.isAntiAlias = true
+        paint.alpha = 0
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
+        paint.style = Paint.Style.FILL
+        paint.isAntiAlias = true
 
 
         top = (resources.getDrawable(images[0]) as BitmapDrawable).bitmap
         bottom = (resources.getDrawable(images[1]) as BitmapDrawable).bitmap
+        rectF = RectF()
+        topCanVas = Canvas()
+        topBg = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.RGB_565)
 
     }
 
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        topCanVas!!.drawBitmap(top!!, null, rectF!!, null)
-        canvas.drawBitmap(bottom!!, null, rectF!!, null)
-        canvas.drawBitmap(topBg!!, null, rectF!!, null)
-        topCanVas!!.drawCircle(mWidth - offsetX, mHeight - offsetY, radius, paint!!)
+        topCanVas.drawBitmap(top, null, rectF, null)
+        canvas.drawBitmap(bottom, null, rectF, null)
+        canvas.drawBitmap(topBg, null, rectF, null)
+        topCanVas.drawCircle(mWidth - offsetX, mHeight - offsetY, radius, paint)
+
 
     }
 
-    @SuppressLint("DrawAllocation")
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         mWidth = View.MeasureSpec.getSize(widthMeasureSpec)
         mHeight = View.MeasureSpec.getSize(heightMeasureSpec)
-        rectF = RectF(0f, 0f, mWidth.toFloat(), mHeight.toFloat())
-        topBg = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_4444)
-        topCanVas = Canvas(topBg!!)
+
+        rectF.left=0f
+        rectF.top =0f
+        rectF.right = mWidth.toFloat()
+        rectF.bottom = mHeight.toFloat()
+
+        topBg.reconfigure(mWidth,mHeight,Bitmap.Config.ARGB_4444)
+        topCanVas.setBitmap(topBg)
     }
+
 
 
     fun getWidthAndHeight() {
         val resources = this.resources
         val dm = resources.displayMetrics
         screenHeight = dm.heightPixels
+        screenWidth = dm.widthPixels
     }
 
     fun setRecyclerView(recycler: RecyclerView) {
@@ -109,7 +118,7 @@ class ScrollChangeView : View {
         //等图全部出来完之后再开始渲染
         if (y > 0 && screenHeight >= height) {
             radius = (screenHeight - height).toFloat()
-            topCanVas!!.drawCircle(this.mWidth - offsetX, this.mHeight - offsetY, radius, paint!!)
+            topCanVas.drawCircle(this.mWidth - offsetX, this.mHeight - offsetY, radius, paint)
         }
         postInvalidate()
     }
